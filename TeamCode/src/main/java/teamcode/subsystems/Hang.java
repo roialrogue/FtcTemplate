d60ba3231d1d4136be98ce2163a9cd575cd361d7
roialrogue <144475675+roialrogue@users.noreply.github.com>
2024-06-05 16:55:27 -0400

    HW Edit

--- TeamCode/src/main/java/teamcode/subsystems/Hang.java
+++ TeamCode/src/main/java/teamcode/subsystems/Hang.java
@@ -6,13 +6,33 @@ import teamcode.RobotParams;
 
 public class Hang
 {
-//    private final TrcMotor hangMotor;
+    private final TrcMotor hangMotor;
 
     public Hang()
     {
-//        FtcMotorActuator.Params slideParams = new FtcMotorActuator.Params()
-//                .setMotorInverted(RobotParams.HANG_MOTOR_INVERTED)
-//                .setLowerLimitSwitch()
+        FtcMotorActuator.Params hangParams = new FtcMotorActuator.Params()
+                .setMotorInverted(RobotParams.HANG_MOTOR_INVERTED)
+                .setLowerLimitSwitch(
+                        RobotParams.HANG_HAS_LOWER_LIMIT_SWITCH,
+                        RobotParams.HANG_LOWER_LIMIT_INVERTED)
+                .setUpperLimitSwitch(
+                        RobotParams.HANG_HAS_UPPER_LIMIT_SWITCH,
+                        RobotParams.HANG_UPPER_LIMIT_INVERTED)
+                .setVoltageCompensationEnabled(RobotParams.HANG_VOLTAGE_COMP_ENABLED)
+                .setPositionScaleAndOffset(RobotParams.HANG_INCHES_PER_COUNT, RobotParams.HANG_OFFSET)
+                .setPositionPresets(RobotParams.HANG_PRESET_TOLERANCE,RobotParams.HANG_PRESETS);
+        hangMotor = new FtcMotorActuator(RobotParams.HWNAME_HANG, hangParams).getActuator();
+        hangMotor.setSoftwarePidEnabled(true);
+        hangMotor.setPositionPidCoefficients(
+                RobotParams.HANG_KP, RobotParams.HANG_KI, RobotParams.HANG_KD, RobotParams.HANG_KF, RobotParams.HANG_IZONE);
+        hangMotor.setPositionPidTolerance(RobotParams.HANG_TOLERANCE);
+        hangMotor.setStallDetectionEnabled(
+                RobotParams.HANG_STALL_DETECTION_DELAY,RobotParams.HANG_STALL_DETECTION_TIMEOUT,RobotParams.HANG_STALL_ERR_RATE_THRESHOLD
+        );
     }
 
+    public TrcMotor getHangMotor()
+    {
+        return hangMotor;
+    }
 }
