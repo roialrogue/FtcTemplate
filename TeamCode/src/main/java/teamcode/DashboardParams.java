f356acb18c4fce5233bd3a8162dd372cf0734238
roialrogue <144475675+roialrogue@users.noreply.github.com>
2024-07-30 16:42:25 -0400

    fixing head

--- TeamCode/src/main/java/teamcode/DashboardParams.java
+++ TeamCode/src/main/java/teamcode/DashboardParams.java
@@ -8,5 +8,5 @@ public class DashboardParams
     public static double elevatorKi = 0.0;
     public static double elevatorKd = 0.0;
     public static double elevatorTol = 0.1;
-    public static double elevatorIZone = 0.0; 
+    public static double elevatorIZone = 0.0;
 }
